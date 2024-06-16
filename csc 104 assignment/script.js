609c574cf7ef30391e7641ef5d5049e12d002996
Gotau-nendang <gotaunendang9@gmail.com>
2024-06-16 12:08:44 +0100

    Add files via upload

---
+++ csc 104 assignment/script.js
@@ -0,0 +1,7 @@
+   let user="WAZI"
+   alert(`welcome to csc 104 ${user}`)
+   console.log(user)
+   let ask = prompt("what day of the week is today?","TUESDAY")
+   alert(ask)
+   let question = confirm("there are seven days in a week t/f")
+   alert(question)
